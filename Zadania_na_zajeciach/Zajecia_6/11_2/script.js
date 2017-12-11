244eb0573e88678b16fd071e36b3df65797063c7
wiktor7245 <wiktor7245@o2.pl>
2017-12-11 16:41:38 +0100

    xd

---
+++ Zadania_na_zajeciach/Zajecia_6/11_2/script.js
@@ -0,0 +1,14 @@
+//var el = document.getElementsByTagName('li');
+
+//calosc wraz z znacznikami
+/*for(var i = 0; i < el.length;i++){
+   //el[i].innerHTML;
+   console.log(el[i]);
+}*/
+
+
+for(var i = 1; i<5;i++){
+  console.log(document.getElementById('li-'+i).innerHTML);
+}
+
+//dorobic druga czesc
